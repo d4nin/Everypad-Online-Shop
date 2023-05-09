664459041098c30b126f64c87d461f3b4d1334b0
Danin Sadzak <daninsadzak@gmail.com>
2023-05-10 00:59:02 +0200

    Napravljena switch case naredba a opciju za profil

--- main.cpp
+++ main.cpp
@@ -6,14 +6,27 @@ using namespace std;
 
 int main() {
     title();
+
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(h, 10);
     cout << setw(25) << "IZABERITE OPCIJU:\n";
     int opcijaZaProfil;
     cout << "1. Registracija\n";
     cout << "2. Prijava\n\n";
-    cout << "(Koristite brojeve izmedju 1 - 2)";
-    cin >> opcijaZaProfil;
+    cout << "(Koristite brojeve izmedju 1 - 2)\n";
+    SetConsoleTextAttribute(h, 7);
+    unos:cin >> opcijaZaProfil;
+    switch(opcijaZaProfil) {
+        case 1:
+            cout << "Lololololol\n";
+            break;
+        case 2:
+            cout << "xdxdxdxdxdxd\n";
+            break;
+        default:
+            cout << "Morate unijeti brojeve izmedju 1 - 2! Pokusaj te ponovo:\n";
+            goto unos;
+    }
     SetConsoleTextAttribute(h, 7);
     return 0;
 }
