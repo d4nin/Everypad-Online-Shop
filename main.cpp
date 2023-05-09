7141d5a1f8ca9878c803088b66d8d7e403149b47
Danin Sadzak <daninsadzak@gmail.com>
2023-05-10 00:56:30 +0200

    Napravljena opcija za registraciju i prijavu

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ int main() {
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(h, 10);
     cout << setw(25) << "IZABERITE OPCIJU:\n";
+    int opcijaZaProfil;
+    cout << "1. Registracija\n";
+    cout << "2. Prijava\n\n";
+    cout << "(Koristite brojeve izmedju 1 - 2)";
+    cin >> opcijaZaProfil;
     SetConsoleTextAttribute(h, 7);
     return 0;
 }
