f84cc812a0b53333b6de50a9fa9892db73f966bd
Danin Sadzak <daninsadzak@gmail.com>
2023-05-10 00:53:20 +0200

    Poceta opcija za registraciju i prijavu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <windows.h>
+#include <iomanip>
 #include "title.h"
 using namespace std;
 
 int main() {
     title();
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 10);
+    cout << setw(25) << "IZABERITE OPCIJU:\n";
+    SetConsoleTextAttribute(h, 7);
     return 0;
 }
