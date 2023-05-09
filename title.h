95bb503ec62349844c3e0c93380bbf42c2e34e93
Danin Sadzak <daninsadzak@gmail.com>
2023-05-09 14:58:15 +0200

    Promjenjena boja u title textu

--- title.h
+++ title.h
@@ -5,12 +5,6 @@ using namespace std;
 void title() {
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(h, 10);
-    cout << " _____                                     _ \n";
-    cout << "|  ___|                                   | |\n";
-    cout << "| |____   _____ _ __ _   _ _ __   __ _  __| |\n";
-    cout << "|  __\ \ / / _ \ '__| | | | '_ \ / _` |/ _` |\n";
-    cout << "| |___\ V /  __/ |  | |_| | |_) | (_| | (_| |\n";
-    cout << "\____/ \_/ \___|_|   \__, | .__/ \__,_|\__,_|\n";
-    cout << "                      __/ | |                \n";
-    cout << "                     |___/|_|                \n";
+    cout << "Everypad";
+    SetConsoleTextAttribute(h, 7);
 }
