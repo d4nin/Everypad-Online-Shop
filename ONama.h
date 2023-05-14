82169eab900f20feeaddcf2967dd045d15ccedc4
Danin Sadzak <daninsadzak@gmail.com>
2023-05-14 16:27:27 +0200

    Poceta radnja na ONama() funkciji

--- ONama.h
+++ ONama.h
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 void ONama() {
+    system("CLS");
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
     
+    SetConsoleTextAttribute(h, 7);
 }
