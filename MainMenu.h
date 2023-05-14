bc508f952d22be34acb29cd71e26504e1fe841da
Danin Sadzak <daninsadzak@gmail.com>
2023-05-14 16:04:41 +0200

    Poceta radnja na mainmenu.h

--- MainMenu.h
+++ MainMenu.h
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 void MainMenu(string korisnicka) {
     system("CLS");
-    cout << "Dobro dosli, " << korisnicka << '\n';
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(h, 2);
+    cout << "Dobro dosli, ";
+    SetConsoleTextAttribute(h, 3);
+    cout << korisnicka << '\n';
+    SetConsoleTextAttribute(h, 7);
 }
