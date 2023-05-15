5b18726d38f436e7aba2e14c2f34c022487b6b8a
Danin Sadzak <daninsadzak@gmail.com>
2023-05-15 11:21:36 +0200

    Poceta radnja na korist() funkciji

--- EverypadKakoKoristi.h
+++ EverypadKakoKoristi.h
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 void korist() {
-    
+    system("CLS");
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    cout << "-------------------------------\n";
+    cout << "|   KAKO KORISTITI EVERYPAD   |\n";
+    cout << "-------------------------------\n\n\n";
 }
