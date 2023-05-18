089e28201385f9d8ff5c3770b50cb9c5bef3de5a
Danin Sadzak <daninsadzak@gmail.com>
2023-05-18 15:27:48 +0200

    Poceta radnja na Kupovina() funkciji

--- Kupovina.h
+++ Kupovina.h
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
 using namespace std;
 
 void Kupovina() {
+    system("CLS");
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 14);
     cout << "Lorem ipsum\n";
 }
