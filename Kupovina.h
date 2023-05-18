c5811889deb5c080563e3afda33b80145df55c4b
Danin Sadzak <daninsadzak@gmail.com>
2023-05-18 15:40:43 +0200

    Napravljena struktura kupovina u Kupovina.h

--- Kupovina.h
+++ Kupovina.h
@@ -3,6 +3,12 @@
 #include <conio.h>
 using namespace std;
 
+struct kupovina {
+    string imeArtikla;
+    int cijena;
+    string kategorijaArtikla;
+};
+
 void Kupovina() {
     system("CLS");
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
