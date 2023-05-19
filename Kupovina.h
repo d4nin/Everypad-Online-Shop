30f5a8311b3aabcb1412b0c692f2f95c9935afcd
Danin Sadzak <daninsadzak@gmail.com>
2023-05-19 23:41:44 +0200

    Napravljena datoteka za proizvode

--- Kupovina.h
+++ Kupovina.h
@@ -2,7 +2,10 @@
 #include <windows.h>
 #include <conio.h>
 #include <string>
+#include <fstream>
 using namespace std;
+
+ofstream proizvodi("proizvodi.txt");
 
 struct kupovina {
     string model;
@@ -28,20 +31,24 @@ void Kupovina() {
         cin.ignore();
         getline(cin, p[i].kategorija);
         cout << '\n';
+        proizvodi << p[i].kategorija << " ";
 
         SetConsoleTextAttribute(h, 10);
         cout << "Unesite model " << i + 1 << ". proizvoda:\n";
         getline(cin, p[i].model);
         cout << '\n';
+        proizvodi << p[i].model << " ";
 
         SetConsoleTextAttribute(h, 11);
         cout << "Unesite marku " << i + 1 << ". proizvoda:\n";
         getline(cin, p[i].brend);
         cout << '\n';
+        proizvodi << p[i].brend << " ";
 
         SetConsoleTextAttribute(h, 9);
         cout << "Unesite cijenu " << i + 1 << ". proizvoda:\n";
         cin >> p[i].cijena;
+        proizvodi << p[i].cijena << 'KM\n';
         system("CLS");
     }
 }
